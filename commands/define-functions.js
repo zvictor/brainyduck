1399542a5627ea398625830de20e7d1bdbb7bd59
zvictor <victor0110@msn.com>
2020-08-03 11:45:53 +0200

    treat .fql files not as JS files - fix #4

--- commands/define-functions.js
+++ commands/define-functions.js
@@ -2,9 +2,11 @@
 
 const fs = require('fs')
 const path = require('path')
+const tempy = require('tempy')
 const figures = require('figures')
 const logSymbols = require('log-symbols')
 const debug = require('debug')('define-functions')
+const faunaEval = require('fauna-shell/src/commands/eval')
 const { Client, query: q } = require('faunadb')
 const { patternMatch } = require('../utils')
 
@@ -34,15 +36,26 @@ const main = async (pattern = '**/*.fql') => {
           .then(() => debug(logSymbols.warning, 'old function deleted'))
       }
 
-      const ref = await client.query(
-        q.CreateFunction({
-          name,
-          body: eval(body),
-        })
-      )
+      const query = `CreateFunction({ name: "${name}", body:\n${body}\n})`
+      debug(`Executing query:\n${query}`)
 
-      debug(`${logSymbols.success} new function created:\n`, ref)
-      return ref
+      const tmpFile = tempy.file()
+      await faunaEval.run([query, '--secret', secret, '--output', tmpFile])
+
+      return new Promise((resolve, reject) => {
+        const stream = fs.createReadStream(tmpFile)
+        const chunks = []
+
+        stream.on('error', reject)
+        stream.on('data', (chunk) => chunks.push(chunk))
+        stream.on('end', () => {
+          const data = JSON.parse(Buffer.concat(chunks).toString('utf8'))
+
+          debug(`${logSymbols.success} function has been created/updated: ${data.name}`)
+
+          resolve(data)
+        })
+      })
     })
   )
 }
