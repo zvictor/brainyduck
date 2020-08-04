052da33642afdbdc9bba68e7a3e21283f17496be
zvictor <victor0110@msn.com>
2020-08-04 09:39:35 +0200

    add a modular schema to push-schema tests

--- tests/specs/push-schema.js
+++ tests/specs/push-schema.js
@@ -2,7 +2,7 @@ import execa from 'execa'
 import { resolve } from 'path'
 import { serial as test } from 'ava'
 
-test('upload a basic schema', async (t) => {
+test('push a basic schema', async (t) => {
   const cwd = resolve(`${__dirname}/../../examples/basic`)
   t.timeout(35000)
 
@@ -16,6 +16,34 @@ test('upload a basic schema', async (t) => {
 \t}
 \ttype Query {
 \t  allUsers: [User!]
+\t}`
+
+  t.true(stderr.includes(mergedSchema))
+  t.is(stdout.split('\n')[0], `Schema imported successfully.`)
+
+  t.false(stdout.includes('error'))
+  t.is(exitCode, 0)
+})
+
+test('push a modular schema', async (t) => {
+  const cwd = resolve(`${__dirname}/../../examples/modularized`)
+  t.timeout(35000)
+
+  const { stdout, stderr, exitCode } = await execa('node', ['../../index.js', 'push-schema'], {
+    cwd,
+  })
+  const mergedSchema = `The resulting merged schema:
+\ttype User {
+\t  name: String!
+\t}
+\ttype Query {
+\t  allPosts: [Post!]
+\t  sayHello(name: String!): String! @resolver(name: "sayHello")
+\t}
+\ttype Post {
+\t  title: String!
+\t  content: String!
+\t  author: User!
 \t}`
 
   t.true(stderr.includes(mergedSchema))
