43cb7446397d7b8166aea4d2b4d2f39642a0de23
zvictor <zvictor@users.noreply.github.com>
2020-10-28 22:55:08 +0100

    point cache to the parent node_modules

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -2,8 +2,11 @@
 // https://npm.community/t/how-can-i-publish-symlink/5599
 
 const fs = require('fs')
-const { locateCache } = require('../utils')
+const findCacheDir = require('find-cache-dir')
 
-fs.symlinkSync(locateCache('sdk.d.ts'), 'index.d.ts', 'file')
+const cache = findCacheDir({ name: 'faugra', cwd: '../..', thunk: true })('sdk.d.ts')
+
+fs.unlinkSync('index.d.ts')
+fs.symlinkSync(cache, 'index.d.ts', 'file')
 
 process.exit(0)
