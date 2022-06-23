1945a157492f25009649bcabcb3f8c3ee08bd4e4
zvictor <zvictor@users.noreply.github.com>
2022-06-23 06:57:24 +0200

    add confirmation prompt to the reset command

--- commands/reset.js
+++ commands/reset.js
@@ -3,11 +3,44 @@
 import fs from 'fs'
 import ora from 'ora'
 import path from 'path'
+import chalk from 'chalk'
+import readline from 'node:readline'
 import { fileURLToPath } from 'url'
 import { runFQL, importSchema } from '../utils.js'
 
+const ALL_TYPES = {
+  functions: true,
+  indexes: true,
+  roles: true,
+  collections: true,
+  schemas: true,
+}
+
 const readScript = (name) =>
   fs.readFileSync(new URL(path.join(`../scripts/`, name), import.meta.url), { encoding: 'utf8' })
+
+const confirm = (types = ALL_TYPES) =>
+  new Promise((resolve) => {
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    })
+
+    const listOfTypes = chalk.red.bold(Object.keys(types).join(', '))
+
+    console.warn(
+      `\n\nYou are about to wipe out all the ${listOfTypes} from the database associated to the key you provided.`
+    )
+    console.warn(`This action is irreversible and might possibly affect production data.\n\n`)
+
+    rl.question(
+      chalk.bold(`Are you sure you want to delete all the ${listOfTypes}? (y/N)`),
+      (answer) => {
+        rl.close()
+        resolve(answer === 'y')
+      }
+    )
+  })
 
 const reset = (type) => {
   const spinner = ora(`Wiping out ${type}...`).start()
@@ -32,15 +65,7 @@ const reset = (type) => {
   }
 }
 
-export default async function main(
-  types = {
-    functions: true,
-    indexes: true,
-    roles: true,
-    collections: true,
-    schemas: true,
-  }
-) {
+export default async function main(types = ALL_TYPES) {
   const _types = Object.keys(types).filter((key) => types[key])
   console.log(`The following types are about to be deleted:`, _types)
 
@@ -63,8 +88,12 @@ export default async function main(
 
 if (process.argv[1] === fileURLToPath(import.meta.url)) {
   ;(async () => {
-    await main()
+    const types =
+      process.argv[2] && Object.fromEntries(process.argv[2].split(',').map((type) => [type, true]))
 
-    console.log(`All reset operations have succeeded.`)
+    if (await confirm(types)) {
+      await main(types)
+      console.log(`All reset operations have succeeded.`)
+    }
   })()
 }
