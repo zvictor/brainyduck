a942407a93c8cb554b04084c95a7ced0d02e3347
zvictor <zvictor@users.noreply.github.com>
2023-06-24 09:06:54 +0200

    remove `return` from top level

--- fetch-ponyfill.cjs
+++ fetch-ponyfill.cjs
@@ -2,13 +2,13 @@ const debug = require('debug')
 
 if (!globalThis.fetch) {
   debug('brainyduck:fetch')(`Native fetch not found. Using node-fetch`)
-  return (module.exports = require('node-fetch'))
+  module.exports = require('node-fetch')
+} else {
+  debug('brainyduck:fetch')(`Using native fetch`)
+  Object.defineProperty(exports, '__esModule', { value: true })
+
+  module.exports = globalThis.fetch
+  module.exports.Headers = globalThis.Headers
+  module.exports.Request = globalThis.Request
+  module.exports.Response = globalThis.Response
 }
-
-debug('brainyduck:fetch')(`Using native fetch`)
-Object.defineProperty(exports, '__esModule', { value: true })
-
-module.exports = globalThis.fetch
-module.exports.Headers = globalThis.Headers
-module.exports.Request = globalThis.Request
-module.exports.Response = globalThis.Response
